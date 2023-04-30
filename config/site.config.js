12772886f72abaa485462ef9e3efb6e414b4d2d4
coderxtg <132077045+coderxtg@users.noreply.github.com>
2023-04-30 15:10:03 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'You Tube',
+      name: 'YouTube',
       link: 'https://www.youtube.com/@coderx1594',
     },
   ],
