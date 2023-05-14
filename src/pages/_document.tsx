46c9fe5878b1d46469e54ac3f012bf992148dbe8
coderxtg <132077045+coderxtg@users.noreply.github.com>
2023-05-14 19:30:07 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,11 +12,10 @@ class MyDocument extends Document {
           <link rel="icon" href="/icons8-f-67.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3672423733931753" crossorigin="anonymous"></script>
           {siteConfig.googleFontLinks.map(link => (
             <link key={link} rel="stylesheet" href={link} />
           ))}
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3672423733931753"
-     crossorigin="anonymous"></script>
         </Head>
         <body>
           <Main />
